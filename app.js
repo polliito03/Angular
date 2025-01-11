8558afaf7844ea49995d08b08f9a18ec645368d0
Renzo V <renzo@gmail.com>
2025-01-10 20:43:12 -0500

    Implemented calculateValue() function

--- app.js
+++ app.js
@@ -4,7 +4,23 @@
 
 angular.module('MyFirstApp', [])
 
-.controller('MyFirstController', function () {
+.controller('MyFirstController', function ($scope) {
+
+$scope.name = "";
+$scope.totalValue = 0;
+
+$scope.calculateValue = function() {
+    var tempValue = calculateString($scope.name);
+    $scope.totalValue = tempValue;
+};
+
+function calculateString(string) {
+    var temp = 0;
+    for (var i=0; i < string.length; i++) {
+        temp += string.charCodeAt(i);
+    }
+    return temp;
+};
 
 });
 
