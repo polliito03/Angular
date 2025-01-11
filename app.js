e9a562bd05145dc466747e331d993a0c87e9e515
Renzo V <renzo@gmail.com>
2025-01-11 17:41:14 -0500

    Implemented initial conversion page

--- app.js
+++ app.js
@@ -5,23 +5,48 @@
 angular.module('MyFirstApp', [])
 
 .controller('MyFirstController', function ($scope) {
+    $scope.cent = 0;
+    $scope.centOut = 32;
+    $scope.fare = 32;
+    $scope.fareOut = 0;
 
-$scope.name = "";
-$scope.totalValue = 0;
+    $scope.calculateCent = function() {
+        if ( ! $scope.cent )
+            $scope.centOut = "";
+        else
+            $scope.centOut = getFahrenheit($scope.cent);
+    };
 
-$scope.calculateValue = function() {
-    var tempValue = calculateString($scope.name);
-    $scope.totalValue = tempValue;
-};
+    $scope.calculateFare = function() {
+        if ( ! $scope.fare )
+            $scope.fareOut = "";
+        else
+            $scope.fareOut = Math.round(getCelsius($scope.fare));
+    };
 
-function calculateString(string) {
-    var temp = 0;
-    for (var i=0; i < string.length; i++) {
-        temp += string.charCodeAt(i);
-    }
-    return temp;
-};
+    function getFahrenheit(string) {
+        var x = parseInt(string, 10);
+        var temp = (x * 9/5) + 32;
+        return temp;
+    };
+
+    function getCelsius(string) {
+        var x = parseInt(string, 10);
+        var temp = (x - 32) * 5/9;
+        return temp;
+    };
 
 });
+/*
+.controller('UpperController', function ($scope, $filter) {
+    $scope.name2 = "uno";
 
+    $scope.upperFunc = function() {
+        var upCase = $filter('uppercase');
+        $scope.name2 = upCase($scope.name2);
+    };
+
+});
+*/
+// END
 }) ();
